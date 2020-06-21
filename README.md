94032db852464ef02e4baeec7f1c8a02fd602008
prudhvirajk <techprudhvi@gmail.com>
2020-06-21 18:32:33 -0400

    initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Prerequisites:
+Node.js
+Check if node.js is installed in your computer. [Learn more about installation](https://nodejs.org/en/download/package-manager/).
+
+npm
+npm (node package manager) is installed with Node.js
+
+## Check the node.js version:
+node -v
+## npm:
+npm -v
+
+# Angular-CLI
+* npm install -g @angular/cli
+
+# Creating todo app
+* ng new todo-app
---
+++ todo-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
